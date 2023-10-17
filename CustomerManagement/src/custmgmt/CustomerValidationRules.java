32e7ccb6f42cb3ca5145e1c648a0a1d3f816bd97
vedanshii <vedanshi2718@gmail.com>
2023-10-18 03:45:24 +0530

    adding cust management.

--- CustomerManagement/src/custmgmt/CustomerValidationRules.java
+++ CustomerManagement/src/custmgmt/CustomerValidationRules.java
@@ -1,8 +1,15 @@
 package custmgmt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomerValidationRules {
 
+ public static void dupli(){
 
+     List<Integer> l = new ArrayList<Integer>();
+     l.add(0,1);
+ }
 
 
 }
